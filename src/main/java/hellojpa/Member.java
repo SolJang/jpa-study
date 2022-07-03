4f27a019a03f907a6e1c5e770bc69f92de86f4fe
jangsol <jangsol96@gmail.com>
2022-07-03 16:22:42 +0900

    영속성 컨텍스트의 이점 1. 1차 캐시 2. 동일성 보장 3. 트랜잭션을 지원하는 쓰기 지연 4. 변경 감지 5. 지연 로딩

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,6 +15,13 @@ public class Member {
     @Column
     private String name;
 
+    public Member(){}
+
+    public Member(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
